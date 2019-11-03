a5128c89ab5a559da6233c11dcdf9941f7fa9a53
Varick Lim <varick@circles.asia>
2019-11-03 22:57:15 +0800

    fix eslint error

--- web/src/Admin/Logs.js
+++ web/src/Admin/Logs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Heading } from "rebass";
 // import { Input } from "@rebass/forms";
