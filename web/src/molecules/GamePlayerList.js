ec605355477ee9459c68424c4a746058eacecdb5
Varick Lim <valishru@hotmail.com>
2020-12-08 01:21:04 +0800

    update ui of game player list

--- web/src/molecules/GamePlayerList.js
+++ web/src/molecules/GamePlayerList.js
@@ -2,41 +2,44 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Box, Flex, Text } from "rebass";
 
-const GamePlayerList = ({ players, viewer, recipientId, gameOver = false }) => {
-  return (
-    <Box variant="container">
-      <Box variant={gameOver ? "orange" : ""}>
-        <Box variant="card.top.xsmall">Players</Box>
-      </Box>
+const GamePlayerList = ({ players, viewer, recipientId, gameOver = false }) => (
+  <Box variant="container">
+    <Box variant={gameOver ? "orange" : ""}>
+      <Box variant="card.top.xsmall">Scoreboard</Box>
+    </Box>
 
-      <Box variant="card.bottom.xsmall">
-        <Box>
-          {players.map((player, index) => {
-            const { _id, score, name, hasAnswered } = player;
-            const isViewer = _id === viewer._id;
-            const isRecipient = _id === recipientId;
-            return (
-              <Flex key={index} variant="row">
-                <Box width={1 / 12}>{score || "0"}</Box>
-                <Box width={8 / 12}>
-                  <Text display="inline" variant={isViewer ? "bold" : ""}>
-                    {name}
+    <Box variant="card.bottom.xsmall">
+      <Box>
+        {players.map((player, index) => {
+          const { _id, score, name, hasAnswered } = player;
+          const isViewer = _id === viewer._id;
+          const isRecipient = _id === recipientId;
+          return (
+            <Flex key={index} justifyContent="space-between" variant="row">
+              <Box>
+                <Text display="inline" variant={isViewer ? "bold" : ""}>
+                  {name}
+                </Text>
+                {/* {isViewer && " (you)"} */}
+                {/* {isRecipient && " (answering)"} */}
+                {/* {isRecipient && <Text variant="tag.small">Answering</Text>} */}
+                {hasAnswered && (
+                  <Text variant="tag.small" bg="blue">
+                    Done
                   </Text>
-                  {/* {isViewer && " (you)"} */}
-                  {/* {isRecipient && " (answering)"} */}
-                  {isRecipient && <Text variant="tag.small">Answering</Text>}
-                </Box>
-                <Box width={3 / 12} textAlign="right">
+                )}
+              </Box>
+              {/* <Box width={3 / 12} textAlign="right">
                   {hasAnswered && "Done"}
-                </Box>
-              </Flex>
-            );
-          })}
-        </Box>
+                </Box> */}
+              <Box>{score || "0"} points</Box>
+            </Flex>
+          );
+        })}
       </Box>
     </Box>
-  );
-};
+  </Box>
+);
 
 GamePlayerList.propTypes = {
   players: PropTypes.arrayOf(
