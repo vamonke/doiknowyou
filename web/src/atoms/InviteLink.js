dd84868c5707bf22fe51770d6bbf989576889cef
Varick Lim <valishru@hotmail.com>
2020-12-09 00:27:14 +0800

    fix invite link icon

--- web/src/atoms/InviteLink.js
+++ web/src/atoms/InviteLink.js
@@ -3,15 +3,14 @@ import { Box, Flex, Text, Button } from "rebass";
 import Icon from "react-eva-icons";
 
 const InviteLink = () => {
-  const [buttonText, setButtonText] = useState("Invite friends");
-  const [buttonIcon, setButtonIcon] = useState("person-add-outline");
+  const [copied, setCopied] = useState(false);
 
   const webShare = () => {
     const link = document.location;
     const data = {
       url: link,
-      text: "Join game",
-      title: "Do I know you?"
+      text: "Let's play a game"
+      // title: "Do I know you?"
     };
     if (navigator.canShare && navigator.canShare(data)) {
       navigator
@@ -31,8 +30,7 @@ const InviteLink = () => {
     // Unsupported on IE but its k
     try {
       await navigator.clipboard.writeText(link);
-      setButtonText("Invite link copied");
-      setButtonIcon("person-add-outline");
+      setCopied(true);
     } catch (err) {
       console.error("Failed to copy: ", err);
     }
@@ -49,16 +47,16 @@ const InviteLink = () => {
         color="orange"
         onClick={webShare}
       >
-        {buttonIcon === "person-add-outline" ? (
-          <Box key="person-add-outline" mb={-1}>
-            <Icon fill="#FA7F00" name="person-add-outline" size="large" />
+        {copied ? (
+          <Box key="link-2-outline" mb={-1}>
+            <Icon fill="#FA7F00" name="link-2-outline" size="large" />
           </Box>
         ) : (
           <Box key="person-add-outline" mb={-1}>
             <Icon fill="#FA7F00" name="person-add-outline" size="large" />
           </Box>
         )}
-        <Text ml={2}>{buttonText}</Text>
+        <Text ml={2}>{copied ? "Invite link copied" : "Invite friends"}</Text>
       </Button>
     </Flex>
   );
