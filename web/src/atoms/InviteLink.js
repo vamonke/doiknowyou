942fcd602658d1d984bc50755790907766eb3add
Varick Lim <varick.lim@grabtaxi.com>
2020-12-31 00:43:48 +0800

    add qr code

--- web/src/atoms/InviteLink.js
+++ web/src/atoms/InviteLink.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Box, Flex, Text, Button } from "rebass";
 import Icon from "react-eva-icons";
+import QRCode from "qrcode.react";
 
 const InviteLink = () => {
   const [copied, setCopied] = useState(false);
+  const link = document.location.href;
 
   const webShare = () => {
-    const link = document.location;
     const data = {
       url: link,
       text: "Let's play a game"
@@ -26,7 +27,7 @@ const InviteLink = () => {
     }
   };
 
-  const copyToClipboard = async link => {
+  const copyToClipboard = async () => {
     // Unsupported on IE but its k
     try {
       await navigator.clipboard.writeText(link);
@@ -37,28 +38,40 @@ const InviteLink = () => {
   };
 
   return (
-    <Flex justifyContent="center">
-      <Button
-        type="button"
-        variant="link"
-        px={3}
-        py={2}
-        my={2}
-        color="orange"
-        onClick={webShare}
-      >
-        {copied ? (
-          <Box key="link-2-outline" mb={-1}>
-            <Icon fill="#FA7F00" name="link-2-outline" size="large" />
-          </Box>
-        ) : (
-          <Box key="person-add-outline" mb={-1}>
-            <Icon fill="#FA7F00" name="person-add-outline" size="large" />
-          </Box>
-        )}
-        <Text ml={2}>{copied ? "Invite link copied" : "Invite friends"}</Text>
-      </Button>
-    </Flex>
+    <>
+      <Flex justifyContent="center">
+        <Button
+          type="button"
+          variant="link"
+          px={3}
+          py={2}
+          my={2}
+          color="orange"
+          onClick={webShare}
+        >
+          {copied ? (
+            <Box key="link-2-outline" mb={-1}>
+              <Icon fill="#FA7F00" name="link-2-outline" size="large" />
+            </Box>
+          ) : (
+            <Box key="person-add-outline" mb={-1}>
+              <Icon fill="#FA7F00" name="person-add-outline" size="large" />
+            </Box>
+          )}
+          <Text ml={2}>{copied ? "Invite link copied" : "Invite friends"}</Text>
+        </Button>
+      </Flex>
+      <Flex justifyContent="center">
+        <Box pb={3}>
+          <QRCode
+            value={link}
+            style={{ maxWidth: "100%", height: "auto", width: 96 }}
+            fgColor="#2C2736"
+            size={1024}
+          />
+        </Box>
+      </Flex>
+    </>
   );
 };
 
